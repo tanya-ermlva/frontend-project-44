2bc1e029fb452067f83f223736538539973bf996
“tanya-ermlva” <hi@ermolaeva.co>
2024-07-10 00:36:20 +0100

    added brain-games code for asking the name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,6 @@
 #!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+console.log(`Hello, ${userName}!`);
