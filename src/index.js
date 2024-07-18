b8d773595988627ad35f9536a7e12177d9e71c64
“tanya-ermlva” <hi@ermolaeva.co>
2024-07-18 12:47:46 +0100

    removed magic number, added gameCycleNum variable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import readlineSync from 'readline-sync';
 
+const gameCycleNum = 3;
+
 // introMessage should be unique for each game
 const gameStructure = (introMessage, getGameData) => {
   // welcoming user to the game
   const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${userName}!\n${introMessage}`);
   // cycle of the game starts, particular game function is being called within the cycle body
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < gameCycleNum; i += 1) {
     // destructuring to get the game data from particular games
     const [challenge, expectedAnswer] = getGameData();
     const userAnswer = readlineSync.question(`Question: ${challenge}\nYour answer: `);
